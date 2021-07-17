d26e854918ed379696b9ef31231b433bc3accd3c
Daniel Bartsch <daniel.bartsch@live.at>
2021-07-17 17:48:49 +0200

    simplify axisrounding and tick calculation

--- axisrounding.ts
+++ axisrounding.ts
@@ -1,4 +1,4 @@
-const STEP = [5, 10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000]
+const STEP = 5
 
 export const roundAxis = ({
   minValue,
@@ -7,27 +7,9 @@ export const roundAxis = ({
   minValue: number
   maxValue: number
 }) => {
-  const maxIsNegative = maxValue < 0
-  const calculatingMaxValue = maxIsNegative ? -maxValue : maxValue
-  const maxStepIndex = STEP.findIndex((value) => calculatingMaxValue < value)
-  const actualMaxStep =
-    STEP[maxStepIndex === -1 || maxStepIndex === 0 ? 0 : maxStepIndex - 1]
-  const maxMethod = maxIsNegative ? "floor" : "ceil"
-  const maxRounded =
-    Math[maxMethod](calculatingMaxValue / actualMaxStep) * actualMaxStep
-
-  const minIsNegative = minValue < 0
-  const calculatingMinValue = minIsNegative ? -minValue : minValue
-  const minStepIndex = STEP.findIndex((value) => calculatingMinValue < value)
-  const actualMinStep =
-    STEP[minStepIndex === -1 || minStepIndex === 0 ? 0 : minStepIndex - 1]
-  const minMethod = minIsNegative ? "ceil" : "floor"
-  const minRounded =
-    Math[minMethod](calculatingMinValue / actualMinStep) * actualMinStep
-
   return {
-    min: minIsNegative ? -minRounded : minRounded,
-    max: maxIsNegative ? -maxRounded : maxRounded,
+    min: Math.floor(minValue / STEP) * STEP,
+    max: Math.ceil(maxValue / STEP) * STEP,
   }
 }
 
@@ -38,34 +20,14 @@ export const getAxisTicks = ({
   minValue: number
   maxValue: number
 }) => {
-  const maxTicks = 10
-
   const steps = []
 
-  if (minValue < 0 && maxValue > 0) {
-    const minSpan = Math.abs(minValue)
-    const minTickStep = STEP.find((step) => minSpan / step <= maxTicks)
-    const minStepCount = minSpan / minTickStep + 1
+  let min = minValue
 
-    for (let index = 0; index < minStepCount; index++) {
-      steps.push(minValue + minTickStep * index)
-    }
-
-    const maxTickStep = STEP.find((step) => maxValue / step <= maxTicks)
-    const maxStepCount = maxValue / maxTickStep + 1
-
-    for (let index = 0; index < maxStepCount; index++) {
-      steps.push(maxTickStep * index)
-    }
-  } else {
-    const span = Math.abs(minValue) + Math.abs(maxValue)
-
-    const tickStep = STEP.find((step) => span / step <= maxTicks)
-    const stepCount = span / tickStep + 1
-    for (let index = 0; index < stepCount; index++) {
-      steps.push(minValue + tickStep * index)
-    }
+  steps.push(min)
+  while (min < maxValue) {
+    min += STEP
+    steps.push(min)
   }
-
   return steps
 }
